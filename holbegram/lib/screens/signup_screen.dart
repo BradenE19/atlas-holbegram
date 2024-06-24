211ba4a05aaa1a7f2b2bd194c28d62260917839e
BradenE19 <6115@holbertonstudents.com>
2024-06-24 12:31:55 -0500

    signup screen

---
+++ holbegram/lib/screens/signup_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final TextEditingController passwordConfirmController;
+  bool passwordVisible;
+
+  SignUp({
+    Key? key,
+    required this.emailController,
+    required this.usernameController,
+    required this.passwordController,
+    required this.passwordConfirmController,
+    this.passwordVisible = true,
+  }) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  void dispose() {
+    widget.emailController.dispose();
+    widget.usernameController.dispose();
+    widget.passwordController.dispose();
+    widget.passwordConfirmController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    widget.passwordVisible;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add your UI elements for sign-up here, e.g., text fields, buttons, etc.
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPicture(
+                      email: widget.emailController.text,
+                      username: widget.usernameController.text,
+                      password: widget.passwordController.text,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Sign up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
